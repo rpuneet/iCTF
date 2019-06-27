10d4b9fd7286bc4733b0b8c4de4be5297d019822
Puneet Rai <puneetrai04@gmail.com>
2019-06-27 23:56:10 +0530

    Trying again

---
+++ clientBuild/build/precache-manifest.011a3eb77dd97407a0de32abc7e27adb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae129b509e0d73de22af760b13a01530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175086cab015771f1412",
+    "url": "/static/css/main.605cf4f2.chunk.css"
+  },
+  {
+    "revision": "ae8ec65193d1596e8381",
+    "url": "/static/js/2.8e1c1ad9.chunk.js"
+  },
+  {
+    "revision": "175086cab015771f1412",
+    "url": "/static/js/main.3913d0b6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
--- server.js
+++ server.js
@@ -46,10 +46,10 @@ app.use("/api/gamesettings", gamesettings);
 // Serve static assets.
 if (process.env.NODE_ENV === "production") {
   // set a static folder.
-  app.use(express.static("client/build"));
+  app.use(express.static("clientBuild/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "clientBuild", "build", "index.html"));
   });
 }
 
